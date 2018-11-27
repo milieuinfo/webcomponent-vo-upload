9227aa37bfcf0e057313919736bf832a19248ec1
Sander Kleykens <sander.kleykens@avnu.be>
2018-11-27 10:22:01 +0100

    GVR-62: events toevoegen en delayed upload introduceren

--- vo-upload.js
+++ vo-upload.js
@@ -11,6 +11,8 @@ import '../../node_modules/dropzone/dist/dropzone.js';
  *
  * Event | Uitleg | Detail object
  * ------|-------------|--------------
+ * `vo-upload-bestand-toegevoegd` | Wordt afgevuurd wanneer er een bestand werd toegevoegd. | { bestand }
+ * `vo-upload-bestand-verwijderd` | Wordt afgevuurd wanneer er een bestand werd verwijderd. | { bestand }
  * `vo-upload-versturen` | Wordt afgevuurd vlak voordat het bestand verstuurd wordt naar de url. | { bestand }
  * `vo-upload-succes` | Wordt afgevuurd wanneer een bestand succesvol werd opgeladen. | { bestand, response }
  * `vo-upload-fout` | Wordt afgevuurd wanneer een bestand niet kon opgeladen worden vanwege een fout. | { bestand, fout }
@@ -27,11 +29,15 @@ class VoUpload extends LitElement {
         super();
         Dropzone.autoDiscover = false;
         Dropzone.prototype.defaultOptions.dictDefaultMessage = 'Sleep bestanden naar hier om op te laden';
+        Dropzone.prototype.defaultOptions.dictCancelUpload = 'Annuleer oplading';
+        Dropzone.prototype.defaultOptions.dictCancelUploadConfirmation = 'Ben je zeker dat je deze oplading wilt annuleren?';
+        Dropzone.prototype.defaultOptions.dictRemoveFile = 'Verwijder bestand';
         Dropzone.prototype.defaultOptions.dictInvalidFileType = 'ONGELDIG_BESTANDSTYPE';
         Dropzone.prototype.defaultOptions.dictFileTooBig = 'BESTAND_TE_GROOT';
         Dropzone.prototype.defaultOptions.dictResponseError = 'HTTP_FOUT_{{statusCode}}';
 
         this.param = this.param || 'file';
+        this['auto-upload'] = this['auto-upload'] !== undefined;
     }
 
     static get properties() {
@@ -55,7 +61,15 @@ class VoUpload extends LitElement {
             /**
              * Het veld in de multipart/form-data body waarin het bestand gestoken moet worden.
              */
-            param: String
+            param: String,
+            /**
+             * Het maximum aantal bestanden dat men mag uploaden.
+             */
+            'maximum-aantal-bestanden': Number,
+            /**
+             * Moet er automatisch worden geupload?
+             */
+            'auto-upload': Boolean
         };
     }
 
@@ -65,12 +79,23 @@ class VoUpload extends LitElement {
     firstUpdated() {
         const self = this;
         const element = this.shadowRoot.querySelector('#upload');
-        this.dropzone = new Dropzone(element, {
+        this._dropzone = new Dropzone(element, {
             url: this.url,
             paramName: this.param,
             maxFilesize: this._dropzoneMaxFilesize(),
             acceptedFiles: this._dropzoneAcceptedFiles(),
+            addRemoveLinks: true,
+            maxFiles: this['maximum-aantal-bestanden'],
+            autoProcessQueue: this['auto-upload'],
             init: function () {
+                this.on('addedfile', function(bestand) {
+                    self._fireEvent('bestand-toegevoegd', { bestand: VoUpload._dropzoneBestandNaarVoUploadBestand(bestand) });
+                });
+
+                this.on('removedfile', function(bestand) {
+                    self._fireEvent('bestand-verwijderd', { bestand: VoUpload._dropzoneBestandNaarVoUploadBestand(bestand) });
+                });
+
                 this.on('sending', function(bestand) {
                     self._fireEvent('versturen', { bestand: VoUpload._dropzoneBestandNaarVoUploadBestand(bestand) });
                 });
@@ -104,11 +129,19 @@ class VoUpload extends LitElement {
 
     static _dropzoneBestandNaarVoUploadBestand(bestand) {
         return {
+            id: bestand.upload.uuid,
             naam: bestand.name,
             grootte: bestand.size,
             type: bestand.type,
             laatstGewijzigd: bestand.lastModified
         }
+    }
+
+    /**
+     * Upload alle bestanden die de gebruiker geselecteerd heeft.
+     */
+    upload() {
+        this._dropzone.processQueue();
     }
 
     /**
@@ -131,6 +164,10 @@ class VoUpload extends LitElement {
     			
     			#upload .dz-image {
     				border-radius: 0px;
+    			}
+    			
+    			#upload .dz-error-message {
+    			    display: none;
     			}
     		</style>
     		<div id="upload" class="dropzone"></div>
